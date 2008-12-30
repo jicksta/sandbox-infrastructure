af49c4894a48072bb6f6cb1a3a0b1d46edd5c3f2
Jay Phillips <jicksta+git@gmail.com>
2008-12-29 20:45:50 -0800

    fleshing out inbound_agi_tunnel and reporter.erl

--- inbound_agi_tunnel/src/inbound_agi_tunnel.erl
+++ inbound_agi_tunnel/src/inbound_agi_tunnel.erl
@@ -2,4 +2,39 @@
 -export([start/0]).
 
 start() ->
-	io:format("Hello world~n").
+	io:format("Starting server~n"),
+	{ok, ServerSocket} = gen_tcp:listen(20000, [list, {packet, 0}, {active, true}]),
+	spawn_link(fun -> receive_connection_loop(ServerSocket) end),
+	loop(fun() ->
+		receive
+			{'EXIT', something}
+		end
+	end).
+
+loop(Fun) -> loop(Fun, []).
+loop(Fun, Args) ->
+	apply(Fun, Args),
+	loop(Fun, Args).
+
+receive_connection_loop(ServerSocket) ->
+	{ok, Socket} = gen_tcp:accept(ServerSocket),
+	handle_connection(self(), Socket),
+	receive_connection_loop(ServerSocket).
+
+handle_connection(Parent, Socket) ->
+	case(authenticate_socket(Socket)) of
+		ok -> wait_for_agi_leg_or_timeout(Socket);
+		Error ->
+			Parent ! {unauthenticated, Socket}
+	end,
+	handle_connection(Parent).
+	
+authenticate_socket(Parent) ->
+	receive
+		{tcp, _Socket, RawAuthentication} ->
+			[LastCharacter|ReversedAuthentication] = lists:reverse(RawAuthentication),
+			case(LastCharacter) of
+				"\n" -> ok
+				_ -> exit({})
+			end
+	end,
---
+++ inbound_agi_tunnel/src/reporter.erl
@@ -0,0 +1,6 @@
+% This module should both log to the console and a log file for debugging purposes.
+
+-module(reporter).
+-export([unauthenticated/1]).
+
+unauthenticated(IP_ADDRESS) -> todo.

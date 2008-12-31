879430ea34971040d0cdcbef93cd4b475344a369
Jay Phillips <jicksta+git@gmail.com>
2008-12-30 20:58:05 -0800

    Fleshed out most of the dialplan for the authenticator app

--- call-authenticator/dialplan.rb
+++ call-authenticator/dialplan.rb
@@ -1,13 +1,23 @@
-adhearsion {
-  user = User.find_by_username extension
-  if user
+sandbox {
+  if SandboxUser.find_by_username(extension)
     variable "SANDBOX_USERNAME" => extension
   else
     +unrecognized_username
   end
 }
 
+adhearsion_not_running_remotely {
+  # "Sorry, we can't find your Adhearsion application running. Please check that you enabled the sandbox component and that your application has already been started."
+  # "For more help, please visit Adhearsion.com. There you'll find links to the wiki, the Adhearsion IRC channel, and the mailing list. Those should be of some assistance if you keep having issues."
+  # "Thanks using Adhearsion. I hope you enjoy using it as much as I enjoy writing it."
+  play 'jay-adhearsion-not-running', 'jay-for-more-help', "jay-thanks-for-using-adhearsion"
+  hangup
+}
+
 unrecognized_username {
-  play 'invalid'
+  # "Sorry, the username you're calling doesn't seem to have been registered yet on Adhearsion.com. Please visit the site, click the Getting Started link, and sign up for an sandbox account."
+  # "For more help, please visit Adhearsion.com. There you'll find links to the wiki, the Adhearsion IRC channel, and the mailing list. Those should be of some assistance if you keep having issues."
+  # "Thanks using Adhearsion. I hope you enjoy using it as much as I enjoy writing it."
+  play 'jay-unrecognized-username', 'jay-for-more-help', "jay-thanks-for-using-adhearsion"
   hangup
 }

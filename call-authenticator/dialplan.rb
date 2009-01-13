839e88f52d28c115c88c2c6218b4ca67b1235850
Jay Phillips <jicksta+git@gmail.com>
2009-01-12 21:14:46 -0800

    Implemented sound files

--- call-authenticator/dialplan.rb
+++ call-authenticator/dialplan.rb
@@ -7,17 +7,17 @@ sandbox {
 }
 
 adhearsion_not_running_remotely {
-  # "Sorry, we can't find your Adhearsion application running. Please check that you enabled the sandbox component and that your application has already been started."
-  # "For more help, please visit Adhearsion.com. There you'll find links to the wiki, the Adhearsion IRC channel, and the mailing list. Those should be of some assistance if you keep having issues."
-  # "Thanks using Adhearsion. I hope you enjoy using it as much as I enjoy writing it."
-  play 'jay-adhearsion-not-running', 'jay-for-more-help', "jay-thanks-for-using-adhearsion"
+  sound_file_dir = "/opt/sandbox-infrastructure/call-authenticator/sound_files"
+  
+  # "Sorry but the sandbox doesn't see your Adhearsion application connected into it. Check that you enabled the sandbox component and that your application has actually been started."
+  # "For more help, please visit Adhearsion.com. There you'll find links to the wiki, the IRC channel, and the mailing list."
+  play "#{sound_file_dir}/jay-adhearsion-not-running", "#{sound_file_dir}/jay-for-more-help"
   hangup
 }
 
 unrecognized_username {
   # "Sorry, the username you're calling doesn't seem to have been registered yet on Adhearsion.com. Please visit the site, click the Getting Started link, and sign up for an sandbox account."
-  # "For more help, please visit Adhearsion.com. There you'll find links to the wiki, the Adhearsion IRC channel, and the mailing list. Those should be of some assistance if you keep having issues."
-  # "Thanks using Adhearsion. I hope you enjoy using it as much as I enjoy writing it."
-  play 'jay-unrecognized-username', 'jay-for-more-help', "jay-thanks-for-using-adhearsion"
+  # "For more help, please visit Adhearsion.com. There you'll find links to the wiki, the IRC channel, and the mailing list."
+  play "#{sound_file_dir}/jay-unrecognized-username", "#{sound_file_dir}/jay-for-more-help"
   hangup
 }
